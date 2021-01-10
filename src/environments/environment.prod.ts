dbbfd24b8c80db3e7e99933a809364360b841b87
VTejeswar <tejeswar.rao000@gmail.com>
2021-01-10 19:09:08 +0530

    s

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:'https://ocademyapiqa.onpassive.com',
-  api:'https://ocademyapiqa.onpassive.com'
-  // api: 'https://ocademyapiuat.onpassive.com'
-};
